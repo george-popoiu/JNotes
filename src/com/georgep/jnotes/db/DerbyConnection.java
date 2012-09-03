3f059c306479fd4843096d7995b2defe4bce2039
george-popoiu <georgepopoiu@yahoo.com>
2012-09-03 12:33:32 +0300

    Corrected bug. The code to check wheter a table exists or not in the db was bugged

--- src/com/georgep/jnotes/db/DerbyConnection.java
+++ src/com/georgep/jnotes/db/DerbyConnection.java
@@ -25,15 +25,18 @@ public class DerbyConnection {
 	private void createSchema() {
 		try {
 			if(conn==null) throw new Exception();
+			conn.setAutoCommit(false);
 			DatabaseMetaData dmd = conn.getMetaData();
-			ResultSet rs = dmd.getTables(null, "APP", NOTES_TABLE, null);
-			if(!rs.next()) {
+			ResultSet rs = dmd.getTables(null, null, null, new String[] {"TABLE"});
+			if(rs.next()==false) {
 				Statement stmt = conn.createStatement();
 				if(stmt==null) throw new Exception();
 				stmt.executeUpdate("CREATE TABLE " + NOTES_TABLE + 
 						"(" + ID_COLUMN + " int, " + TEXT_COLUMN +" CLOB)");
+				stmt.executeUpdate("INSERT INTO " + NOTES_TABLE + " VALUES(1, '')");
 				stmt.close();
 			}
+			rs.close();
 		}
 		catch(Exception e) {
 			e.printStackTrace(System.err);
@@ -68,7 +71,7 @@ public class DerbyConnection {
 			}
 		}
 		catch(Exception e) {
-			e.printStackTrace(System.err);
+			//e.printStackTrace(System.err);
 		}
 	}
 
