7f5391650e9329a6d2c6e0da5d6bf08d3ab0366a
george-popoiu <georgepopoiu@yahoo.com>
2012-09-04 12:27:46 +0300

    Added progress dialog while the app is waiting for database connection

--- src/com/georgep/jnotes/db/DerbyConnection.java
+++ src/com/georgep/jnotes/db/DerbyConnection.java
@@ -1,5 +1,6 @@
 package com.georgep.jnotes.db;
 
+import java.awt.Toolkit;
 import java.io.Console;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
@@ -8,7 +9,11 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import static com.georgep.jnotes.db.DbConstants.*;
 
+import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+
+import com.georgep.jnotes.ProgressDialog;
+
 
 public class DerbyConnection {
 	
@@ -25,12 +30,11 @@ public class DerbyConnection {
 	private void createSchema() {
 		try {
 			if(conn==null) throw new Exception();
-			conn.setAutoCommit(false);
+			conn.setAutoCommit(true);
 			DatabaseMetaData dmd = conn.getMetaData();
 			ResultSet rs = dmd.getTables(null, null, null, new String[] {"TABLE"});
 			if(rs.next()==false) {
-				Statement stmt = conn.createStatement();
-				if(stmt==null) throw new Exception();
+				Statement stmt = conn.createStatement();			
 				stmt.executeUpdate("CREATE TABLE " + NOTES_TABLE + 
 						"(" + ID_COLUMN + " int, " + TEXT_COLUMN +" CLOB)");
 				stmt.executeUpdate("INSERT INTO " + NOTES_TABLE + " VALUES(1, '')");
@@ -46,17 +50,30 @@ public class DerbyConnection {
 		}
 	}
 	
+	Runnable dbconnection = new Runnable() {
+		@Override
+		public void run() {
+			try {
+				Class.forName(DbConstants.DB_DRIVER).newInstance();
+				conn = DriverManager.getConnection(DbConstants.DB_URL);
+			}
+			catch(Exception e) {
+				e.printStackTrace(System.err);
+				JOptionPane.showMessageDialog(null, "Cannot connect to the local database", 
+						"Error", JOptionPane.ERROR_MESSAGE);
+				System.exit(0);
+			}
+		}
+	};
+	
 	private void createConnection() {
-		try {
-			Class.forName(DbConstants.DB_DRIVER).newInstance();
-			conn = DriverManager.getConnection(DbConstants.DB_URL);
-		}
-		catch(Exception e) {
-			e.printStackTrace(System.err);
-			JOptionPane.showMessageDialog(null, "Cannot connect to the local database", 
-					"Error", JOptionPane.ERROR_MESSAGE);
-			System.exit(0);
-		}
+		ProgressDialog pd = new ProgressDialog(new JFrame(), dbconnection, 
+				"Connecting to the database. \n Please wait.");
+		pd.setSize(330, 60);
+		int sw = Toolkit.getDefaultToolkit().getScreenSize().width;
+		int sh = Toolkit.getDefaultToolkit().getScreenSize().height;
+		pd.setLocation(sw/2 - pd.getWidth()/2, sh/2 - pd.getHeight()/2);
+		pd.setVisible(true);
 	}
 	
 	public Connection getConnection() {
@@ -76,3 +93,4 @@ public class DerbyConnection {
 	}
 
 }
+
